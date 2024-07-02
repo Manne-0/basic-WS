2cf8c261a14ccf7fa829a28e56e9515667b1496e
Manne-0 <okoromannie@gmail.com>
2024-07-02 17:56:53 +0100

    testing returned ip address

--- app.py
+++ app.py
@@ -46,33 +46,39 @@ def get_weather(lat, lon):
 
 @app.route('/api/hello', methods=['GET'])
 def greet():
-    visitor_name = request.args.get('visitor_name', default='Guest')
-    client_ip = get_client_ip()
-    location = get_location(client_ip)
-    # return jsonify(client_ip, location)
-
-    if 'loc' in location:
-        lat, lon = location['loc'].split(',')
-        # lat = location['latitude']
-        # lon = location['longitude']
-        city = location['city']
-        # region = location['region']
-        # country = location['country']
-        
-        weather = get_weather(lat, lon)
-        if 'cod' in weather and weather['cod'] == 200:
-            temperature = weather['main']['temp']
-            response = {
-                        "client_ip": f'{client_ip}',
-                        "location": f'{city}',
-                        "greeting": f'Hello, {visitor_name}!, the temperature is {temperature} degrees celsius in {city}'}
-        else:
-            response = {"error": 'could not retrieve temperature data'}
+    if request.headers.getlist("X-Forwarded-For"):
+        ip = request.headers.getlist("X-Forwarded-For")[0]
+        return jsonify({'source': "this ip came from requests.headers", 'ip':ip})
     else:
-        response = {"error": 'Could not retrieve location',
-                    "return_location": f'{location}',
-                    "client_ip": f'{client_ip}'}
-    return jsonify(response)
+        ip = request.remote_addr
+        return jsonify({'source': 'this is from remote_addr', 'ip':ip})
+    # visitor_name = request.args.get('visitor_name', default='Guest')
+    # client_ip = get_client_ip()
+    # location = get_location(client_ip)
+    # # return jsonify(client_ip, location)
+
+    # if 'loc' in location:
+    #     lat, lon = location['loc'].split(',')
+    #     # lat = location['latitude']
+    #     # lon = location['longitude']
+    #     city = location['city']
+    #     # region = location['region']
+    #     # country = location['country']
+        
+    #     weather = get_weather(lat, lon)
+    #     if 'cod' in weather and weather['cod'] == 200:
+    #         temperature = weather['main']['temp']
+    #         response = {
+    #                     "client_ip": f'{client_ip}',
+    #                     "location": f'{city}',
+    #                     "greeting": f'Hello, {visitor_name}!, the temperature is {temperature} degrees celsius in {city}'}
+    #     else:
+    #         response = {"error": 'could not retrieve temperature data'}
+    # else:
+    #     response = {"error": 'Could not retrieve location',
+    #                 "return_location": f'{location}',
+    #                 "client_ip": f'{client_ip}'}
+    # return jsonify(response)
 
     
  
